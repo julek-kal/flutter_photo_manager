547b4bed8e4930d41c9f61702752752a69da4456
Julian Kalinowski <julian.kalinowski@softiero.com>
2020-08-21 17:43:05 +0200

    change subtype in getRecentCollection

--- .history/ios/Classes/PhotoChangeObserver_20200813170623.m
+++ .history/ios/Classes/PhotoChangeObserver_20200813170623.m
@@ -40,7 +40,7 @@
 
 - (PHAssetCollection *)getRecentCollection {
     if (PHPhotoLibrary.authorizationStatus == PHAuthorizationStatusAuthorized) {
-        PHFetchResult<PHAssetCollection *> *result = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+        PHFetchResult<PHAssetCollection *> *result = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAny options:nil];
 
         for (PHAssetCollection *collection in result) {
             if (collection.assetCollectionSubtype == PHAssetCollectionSubtypeSmartAlbumUserLibrary) {
